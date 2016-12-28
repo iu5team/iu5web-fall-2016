f73b7d3f2ea8f8f2f098148f6a9ec8c736836111
alekseyl1992 <alekseyl@list.ru>
2016-12-28 23:33:01 +0300

    ExamTicketGen now uses txt files as input, see example in data/questions.txt

--- tools/ExamTicketGen/gen.py
+++ tools/ExamTicketGen/gen.py
@@ -1,5 +1,8 @@
-import xml.etree.ElementTree as ET
+import argparse
+from enum import Enum
+
 import numpy as np
+import re
 from docxtpl import DocxTemplate
 
 
@@ -13,12 +16,15 @@ def array_split(arr, chunks):
     return result
 
 
-def print_to_stdout(formal_questions):
-    for i, fq in enumerate(formal_questions):
-        print(i + 1)
-        for q in fq:
-            print('{}: {} {}'.format(q['group'], q['title'], q['text']))
-        print('-----')
+def print_to_stdout(tickets):
+    for i, ticket in enumerate(tickets):
+        print('Ticket {}'.format(i))
+        for j, fq in enumerate(ticket['formal_questions']):
+            print(j + 1)
+            for q in fq:
+                print('{}: {} {}'.format(q['group'], q['title'], q['text']))
+            print('-----')
+        print('')
 
 
 def save_to_docx(tickets, template_file, output_file):
@@ -30,21 +36,19 @@ def save_to_docx(tickets, template_file, output_file):
     doc.save(output_file)
 
 
-def generate(xml_file, template_file, output_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    groups = root.find('groups')
-    meta = root.find('meta')
+def generate(args, questions_file, template_file, output_file):
+    groups = parse_questions(questions_file)
 
-    questions_per_group = int(meta.attrib.get('questions_per_group', 1))
-    groups_count = int(meta.attrib.get('groups_count', len(groups)))
-    formal_questions_count = int(meta.attrib.get('formal_questions_count', groups_count))
-    tickets_count = int(meta.attrib.get('tickets_count', 30))
+    if args.groups_count is None:
+        args.groups_count = len(groups)
+
+    if args.formal_questions_count is None:
+        args.formal_questions_count = args.groups_count
 
     tickets = []
-    for i in range(tickets_count):
+    for i in range(args.tickets_count):
         selected_groups = []
-        ids = np.random.choice(len(groups), groups_count, False)
+        ids = np.random.choice(len(groups), args.groups_count, False)
         ids.sort()
         for _id in ids:
             selected_groups.append(groups[_id])
@@ -52,30 +56,134 @@ def generate(xml_file, template_file, output_file):
         # collect all questions to one list
         questions = []
         for group in selected_groups:
-            group_title = group.attrib['title']
             questions_in_group = len(group)
-            group_questions = group.findall('question')
-            ids = np.random.choice(len(group_questions), min(questions_in_group, questions_per_group), False)
-            for _id in ids:
-                title = group_questions[_id].attrib['title']
-                text = group_questions[_id].text or ''
-                questions.append({
-                    'title': title,
-                    'text': text.strip(),
-                    'group': group_title
-                })
+            group_questions = group['questions']
+            ids = np.random.choice(
+                len(group_questions),
+                min(questions_in_group,
+                    args.questions_per_group),
+                False)
 
-        formal_questions = array_split(questions, formal_questions_count)
+            for _id in ids:
+                questions.append(group_questions[_id])
+
+        formal_questions = array_split(questions, args.formal_questions_count)
         tickets.append({
             'formal_questions': formal_questions
         })
 
-    # print_to_stdout(formal_questions)
+    # print_to_stdout(tickets)
     save_to_docx(tickets, template_file, output_file)
 
 
+def parse_questions(questions_file):
+    class State(Enum):
+        BEGIN = 0
+        GROUP_TITLE = 1
+        TITLE = 2
+        QUESTION = 3
+        EMPTY_LINE = 4
+
+    state = State.BEGIN
+
+    groups = []
+
+    question = {}
+    group = {}
+
+    title_regexp = re.compile('\d+\. ')
+
+    def save_question():
+        nonlocal question
+        question['group'] = group['title']
+        group['questions'].append(question)
+        question = {}
+
+    def create_question(title):
+        if not title.endswith('.'):
+            title += '.'
+        question['title'] = title_regexp.sub('', title)
+        question['text'] = ''
+
+    def save_group():
+        nonlocal group
+        groups.append(group)
+        group = {}
+
+    def create_group(title):
+        group['title'] = title
+        group['questions'] = []
+
+    with open(questions_file, encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+
+            if state == State.BEGIN:
+                create_group(line)
+                state = State.GROUP_TITLE
+            elif state == State.GROUP_TITLE:
+                create_question(line)
+                state = State.TITLE
+            elif state == State.TITLE:
+                if title_regexp.match(line):
+                    # new question
+                    save_question()
+                    create_question(line)
+                    state = State.TITLE
+                else:
+                    question['text'] = line
+                    state = State.QUESTION
+            elif state == State.QUESTION:
+                if len(line) == 0:
+                    save_question()
+                    state = State.EMPTY_LINE
+                elif title_regexp.match(line):
+                    save_question()
+                    create_question(line)
+                    state = State.TITLE
+                else:
+                    question['text'] += '\n' + line  # multi-line question
+                    state = State.QUESTION
+            elif state == State.EMPTY_LINE:
+                if len(line) == 0:
+                    pass
+                elif title_regexp.match(line):
+                    create_question(line)
+                    state = State.TITLE
+                else:
+                    save_group()
+                    create_group(line)
+                    state = State.GROUP_TITLE
+            else:
+                raise NotImplementedError
+
+    if question['title']:
+        save_question()
+
+    if group['title']:
+        save_group()
+
+    return groups
+
+
 def main():
-    generate(open('data/pool.xml', encoding='utf8'), 'data/template.docx', 'data/tickets.docx')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--questions_per_group', type=int,
+                        default=1,
+                        help='amount of questions to peek from each group, default = 1')
+    parser.add_argument('--groups_count', type=int,
+                        default=None,
+                        help='amount of groups to peek from, default - all groups')
+    parser.add_argument('--formal_questions_count', type=int,
+                        default=None,
+                        help='amount of items per ticket (one item can contain 1 ot more questions), '
+                             'default = groups_count')
+    parser.add_argument('--tickets_count', type=int,
+                        default=30,
+                        help='total amount of tickets, default = 30')
+    args = parser.parse_args()
+
+    generate(args, 'data/questions.txt', 'data/template.docx', 'data/tickets.docx')
 
 
 if __name__ == '__main__':
